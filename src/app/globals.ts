6b0d5b04b96fc04a8adcea958b576badd00c8074
Oscar Del Aguila Gimenez <oscar1delaguila@gmail.com>
2023-01-04 11:23:33 +0100

    v1.1

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,8 +1,8 @@
 
 
-export const server = "http://localhost:8000";
+//export const server = "http://localhost:8000";
 
-//export const server = "https://gegantoys.com/backend/public";
+export const server = "https://gegantoys.com/backend/public";
 export const sep = '/';
 export const emailAdmin = 'gegantoys@gegantoys.com';
 export const itemsPorPagina = 12;
